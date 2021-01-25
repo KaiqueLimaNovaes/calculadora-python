e2dc645ce4b1218c1d09581c1e72e5424c0661c8
KaqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-25 16:58:19 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+import os
+
+try:
+    from SimpleHTTPServer import SimpleHTTPResquestHandler as Handler
+    from SocketServer import TCPServer as Server
+except ImportError:
+    from http.server import SimpleHTTPRequestHandler as Handler
+    from http.server import HTTPServer as Server
+
+PORT = int(os.getenv('Port', 8000))
+
+os.chdir('static')
+
+httpd = Server(('', PORT), Handler)
+try:
+    print('Start serving at port %i'%PORT)
+    httpd.serve_forever()
+except  KeyboardInterrupt:
+    pass
+httpd.server_close()
