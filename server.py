20651a9a18c9fc58b5bb8d038b9df3fe2d3544d7
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-04 11:40:58 -0300

    Att subtração

--- server.py
+++ server.py
@@ -7,12 +7,18 @@ def home():
        if(request.method == 'GET'):
            return render_template('index.html');
        else:
+           if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'sub'):
+               subtracao = int(request.form['num1']) - int(request.form['num2'])
+               return '<h1>A subtração entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(subtracao)+'</h1>'
+           
            if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'soma'):
                soma = int(request.form['num1']) + int(request.form['num2'])
                return '<h1>A soma entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(soma)+'</h1>'
+           
            if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'mult'):
                multiplicacao = int(request.form['num1']) * int(request.form['num2'])
                return '<h1>A multiplicação entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(multiplicacao)           
+           
            if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'div'):
                divisao = int(request.form['num1']) / int(request.form['num2'])
                return '<h1>A divisão entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(divisao)+'</h1>'
