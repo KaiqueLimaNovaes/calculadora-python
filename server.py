0ee87b9be498cc2394b6d1e987de26557b6676ea
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-02-04 11:08:47 -0300

    Att calculadora

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
-import os
+from flask import Flask, render_template, request
+porta = 777;
+app = Flask (__name__, template_folder='./static')
 
-try:
-    from SimpleHTTPServer import SimpleHTTPResquestHandler as Handler
-    from SocketServer import TCPServer as Server
-except ImportError:
-    from http.server import SimpleHTTPRequestHandler as Handler
-    from http.server import HTTPServer as Server
+@app.route('/', methods=['GET', 'POST'])
+def home():
+       if(request.method == 'GET'):
+           return render_template('index.html');
+       else:
+           if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'soma'):
+               soma = int(request.form['num1']) + int(request.form['num2'])
+               return '<h1>A soma entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(soma)+'</h1>'
+           if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'mult'):
+               multiplicacao = int(request.form['num1']) * int(request.form['num2'])
+               return '<h1>A multiplicação entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(multiplicacao)           
+           if(request.form['num1'] != "" and request.form['num2'] != "" and request.form['escolha'] == 'div'):
+               divisao = int(request.form['num1']) / int(request.form['num2'])
+               return '<h1>A divisão entre '+ str(request.form['num1']) + ' e ' + str(request.form['num2']) + ' é: ' + str(divisao)+'</h1>'
+           
+           else:
+               return 'Informe um valor válido!'
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('index.html')
 
-PORT = int(os.getenv('Port', 8000))
-
-os.chdir('static')
-
-httpd = Server(('', PORT), Handler)
-try:
-    print('Start serving at port %i'%PORT)
-    httpd.serve_forever()
-except  KeyboardInterrupt:
-    pass
-httpd.server_close()
+app.run(port=porta, debug=True)
